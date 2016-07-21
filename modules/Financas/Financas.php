7c587cb8e19a1591bc1c13e44727889f82f404e1
Edwino Stein <edwino.stein@gmail.com>
2016-07-20 22:17:42 -0400

    Adicionando objeto de gerenciamento de sessão do usuário

--- modules/Financas/Financas.php
+++ modules/Financas/Financas.php
@@ -1,8 +1,10 @@
 <?php
 namespace Financas;
 use Application\Application;
+use Financas\Util\User;
 
 class Financas extends Application{
+    protected $user;
     protected $config = array(
         'db' => array(
             'driver' => 'pgsql',
@@ -18,4 +20,13 @@ class Financas extends Application{
         ),
         'defaultController' => 'main'
     );
+
+    protected function init(){
+        $this->user = User::init('user');
+        parent::init();
+    }
+
+    public function user(){
+        return $this->user;
+    }
 }
