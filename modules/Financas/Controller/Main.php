e1e46503d34a7f49f69469329bb2cd17026c180b
Edwino Stein <edwino.stein@gmail.com>
2016-07-20 22:22:24 -0400

    Adicionando suporte a login e logout de usuários

--- modules/Financas/Controller/Main.php
+++ modules/Financas/Controller/Main.php
@@ -1,10 +1,60 @@
 <?php
 namespace Financas\Controller;
 use Application\AbstractController;
+use Financas\Model\Usuario;
 
 class Main extends AbstractController{
 
     public function indexAction(){
         return self::getView(array());
+    }
+
+    public function loginAction(){
+
+        $session = $this->app()->user();
+        if(!$session->isGuest()) return self::getView(array(
+            'error' => false,
+            'message' => "Você já está logado como  <b>".$session->getData('nome')."</b>."
+        ));
+
+        $login = $this->app()->request()->getPost('user-login', null);
+        if(empty($login)) return self::getView(array(
+            'error' => true,
+            'message' => "O campo <b>Login</b> deve ser preenchido."
+        ));
+
+        $password = $this->app()->request()->getPost('user-passwd', null);
+        if(empty($password)) return self::getView(array(
+            'error' => true,
+            'message' => "O campo <b>Senha</b> deve ser preenchido."
+        ));
+
+        $user = Usuario::findOneBy(array('login' => $login));
+        if($user === null) return self::getView(array(
+            'error' => true,
+            'message' => "Usuário informado é inválido."
+        ));
+
+        if(!$user->matchSenha($password)) return self::getView(array(
+            'error' => true,
+            'message' => "A senha informada é inválida."
+        ));
+
+        $session->setData(array(
+            'id' => $user->getId(),
+            'nome' => $user->getNome(),
+            'sobrenome' => $user->getSobrenome(),
+            'login' => $user->getLogin()
+        ));
+
+        return self::getView(array(
+            'error' => false,
+            'message' => "Bem vindo <b>".$user->getNome()."</b>."
+        ));
+    }
+
+    public function logoutAction(){
+        $this->app()->user()->clean();
+        $this->app()->redirect($this->request()->getBaseUri().'index.php');
     }
 }
