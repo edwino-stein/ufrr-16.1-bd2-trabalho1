2f4405142515304ce6a54ddee905970182a38d43
Edwino Stein <edwino.stein@gmail.com>
2016-07-30 21:09:55 -0400

    Adicionando suporte a remoção de receitas/despesas fixas

--- modules/Financas/Controller/Fixos.php
+++ modules/Financas/Controller/Fixos.php
@@ -135,4 +135,39 @@ class Fixos extends AbstractController{
             'messages' => array('generic' => "A receita/despesa foi salva com sucesso.")
         ), 'fixos/form.phtml');
     }
+
+    public function deleteAction(){
+
+        $session = $this->app()->user();
+        if($session->isGuest())
+            $this->app()->redirect($this->request()->getBaseUri().'index.php');
+
+        $id = (int) $this->app()->request()->getQuery('fixo_id', 0);
+        if($id <= 0)
+            $this->app()->redirect($this->request()->getBaseUri().'index.php/fixos/index');
+
+        $model = DispesaReceitaFixa::findOneBy(array('id' => $id, 'usuario' => $session->getData('id')));
+        if($model === null)
+            $this->app()->redirect($this->request()->getBaseUri().'index.php/fixos/index');
+
+        $error = false;
+        $message = '';
+
+        if($this->app()->request()->hasPost('fixo-delete')){
+            try {
+                $model->delete();
+            } catch (\Exception $e) {
+                $error = true;
+                $message = 'Não foi possivel remover a despesa/receita.';
+            }
+
+            $model = null;
+        }
+
+        return self::getView(array(
+            'error' => false,
+            'data' => $model,
+            'messages' => $message
+        ));
+    }
 }
