6ca2457a6f52ad4c86fa9269cb30d88352dd14f0
Edwino Stein <edwino.stein@gmail.com>
2016-08-08 22:52:39 -0400

    Adicionando método getCurrentMes em Financas\Model\Financa

--- modules/Financas/Model/Financa.php
+++ modules/Financas/Model/Financa.php
@@ -95,4 +95,45 @@ class Financa extends ModelBase{
 
         return $query->fetch(\PDO::FETCH_OBJ)->init_mes == 1;
     }
+
+    public static function getCurrentMes($usuario){
+
+        if(!Connection::isInited()) throw Errors::getException(Errors::CONNECTION_NOT_SETTED);
+
+        $modelName = get_called_class();
+        $tableSchema = self::getTableSchema($modelName);
+        $usuario = $usuario instanceof Usuario ? $usuario->getId() : (int) $usuario;
+        $query = Connection::getConnection()->createQuery('SELECT * FROM financas WHERE financas.usuario_id = '.$usuario.' and EXTRACT(MONTH FROM financas.mes) = EXTRACT(MONTH FROM now()) and EXTRACT(YEAR FROM financas.mes) = EXTRACT(YEAR FROM now())');
+
+        //Executa a query ou captura o erro causado
+        try{
+            $query->execute();
+        }
+        catch(\Exception $e){
+            $code = 0;
+            switch ($e->getCode()) {
+                case '42S02':
+                    $code = Errors::TABLE_NOT_FOUND;
+                break;
+
+                case '42S22':
+                    $code = Errors::COLUMN_NOT_FOUND;
+                break;
+
+                case '42000':
+                    $code = Errors::SYNTAX_ERROR;
+                break;
+
+                default:
+                    $code = Errors::UNKNOW_ERROR;
+                break;
+            }
+
+            throw Errors::getException($code, $e);
+        }
+
+        $row = $query->fetch(\PDO::FETCH_OBJ);
+        return $row ? self::getModelInstance($modelName, $tableSchema, $row) : null;
+    }
+
 }
